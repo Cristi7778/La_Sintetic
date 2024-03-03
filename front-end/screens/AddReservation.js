e7358ee27101a68891049b3226b172da69aaf7e3
cristi7778 <cristian.david7778@yahoo.com>
2024-03-03 12:25:34 +0200

    Added DatePicker

--- front-end/screens/AddReservation.js
+++ front-end/screens/AddReservation.js
@@ -1,9 +1,35 @@
-import {SafeAreaView, Text, View } from 'react-native';
+import {SafeAreaView, Text, View,TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function AddReservations({route}) {
+  const [date, setDate] = useState(new Date());
+  const [show,setShow]=useState(false);
+  const onChange = (e, selectedDate) => {
+    setShow(false);
+    setDate(selectedDate);
+
+  };
   return (
     <SafeAreaView >
+      <View>
       <Text>{route.params.item.name}</Text>
+      </View>
+      <View>
+      {show &&<DateTimePicker
+        value={date}
+        mode={"date"}
+        is24Hour={true}
+        onChange={onChange}
+      />
+      }
+      <TouchableOpacity onPress={()=>{setShow(true)}}>
+      <Text>{date.toDateString()}</Text>
+      </TouchableOpacity>
+      
+      
+      </View>
     </SafeAreaView>
+      
   );
-}
+};
