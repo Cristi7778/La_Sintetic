e3d473fdd9d049ec09c281a9775161e808770c26
cristi7778 <cristian.david7778@yahoo.com>
2024-03-08 20:25:39 +0200

    Reservations show+clients can delete them

--- front-end/components/ReservationCard.js
+++ front-end/components/ReservationCard.js
@@ -1,12 +1,41 @@
 import {StyleSheet, Text, View } from 'react-native';
 import {globalStyles} from '../styles/globalStyles';
+import { useEffect, useState } from 'react';
 
 export default function ReservationCard({item}){
+    const date=item.Date.substring(0, 10);
+    const hour=item.Date.substring(11,16);
+    const [location,setLocation]=useState();
+    const [name,setName]=useState();
+    const getDetails = async () => {
+        try {
+          const response = await fetch(
+            `http://192.168.0.101:8080/pitches/${item.PitchId}`,
+           
+          );
+          const json = await response.json();
+          if(response.status===200){
+            setName(json.pitch.name);
+            setLocation(json.pitch.location);
+          }
+          else{
+            if(response.status===404){
+              console.log("Reservations not found");
+            }
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+    useEffect(() => {
+        getDetails();
+      }, []);
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.paragraph}>Location</Text>
-            <Text style={globalStyles.paragraph}>Date</Text>
-            <Text style={globalStyles.paragraph}>Time</Text>
+            <Text style={globalStyles.paragraph}>{name}</Text>
+            <Text style={globalStyles.paragraph}>{location}</Text>
+            <Text style={globalStyles.paragraph}>{date}</Text>
+            <Text style={globalStyles.paragraph}>{hour}</Text>
         </View>
     )
 };
--- front-end/screens/UserReservations.js
+++ front-end/screens/UserReservations.js
@@ -1,11 +1,65 @@
-import {SafeAreaView, Text, View } from 'react-native';
+import {SafeAreaView, Text, View,FlatList,TouchableOpacity,Alert,StyleSheet} from 'react-native';
 import { UserContext } from '../contexts/UserContext';
-import { useContext } from 'react';
+import { useContext, useState,useEffect } from 'react';
+import ReservationCard from '../components/ReservationCard';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 export default function UserReservations() {
   const {user}=useContext(UserContext);
+  const [reservations,setReservations]=useState();
+  const getReservations = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.0.101:8080/reservations`,
+       
+      );
+      const json = await response.json();
+      if(response.status===200){
+       const filteredReservations = json.records.filter(entry =>{
+          return entry.UserUsername===user;
+       })
+       setReservations(filteredReservations);
+      }
+      else{
+        if(response.status===404){
+          console.log("Reservations not found");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+useEffect(() => {
+  getReservations();
+}, );
   return (
     <SafeAreaView >
-      <Text>{user}</Text>
+      <FlatList data={reservations} renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <ReservationCard item={item} />
+          <TouchableOpacity onPress={()=> Alert.alert('Confirm deletion', `Are you sure you want to delete this reservation?`,
+           [{text: 'YES', onPress: () => fetch(`http://192.168.0.101:8080/reservations/${item.ID}`, {method: "DELETE",headers: 
+           {"Content-type": "application/json; charset=UTF-8"}})},
+            {text:'NO'}])}>
+            <MaterialCommunityIcons style={styles.icon} name="cancel" size={24} color="red" />
+          </TouchableOpacity>
+          
+        </View>
+      )} />
     </SafeAreaView>
   );
 }
+const styles=StyleSheet.create(
+  { 
+    itemContainer: {
+    flex:1,
+    flexDirection:'row',
+    
+  },
+
+    icon:{
+      padding:20,
+      textAlign:'center',
+      justifyContent:'center',
+    },
+  
+});
