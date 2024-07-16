f83c4a704b93271145a31fd6521869131dfd0b7d
cristi7778 <cristian.david7778@yahoo.com>
2024-07-16 19:10:24 +0300

    Implemented sorting options

--- front-end/screens/Main.js
+++ front-end/screens/Main.js
@@ -1,8 +1,8 @@
-import {SafeAreaView,FlatList,TouchableOpacity,Text } from 'react-native';
+import {SafeAreaView,FlatList,TouchableOpacity,Text,StyleSheet } from 'react-native';
 import PitchCard from '../components/PitchCard';
 import { useState,useEffect } from 'react';
 import ip from '../global/ip';
-
+import RNPickerSelect from 'react-native-picker-select';
 export default function Main({navigation}) {
 
   const [pitches, setPitches] = useState([
@@ -10,7 +10,19 @@ export default function Main({navigation}) {
     { name: 'ACS NEW TEAM', location: 'DTR2', rate: 200},
     { name: 'TEREN GORJULUI', location: 'DTR3', rate: 200},
   ]);
-
+  const sortChange = (value)=>{
+    console.log(value)
+    console.log(pitches)
+    if(value=='rating'){
+      sortedPitches = [...pitches].sort((a, b) => b.rating - a.rating);
+     
+    }
+    if(value=='price'){
+      sortedPitches = [...pitches].sort((a, b) => a.rate - b.rate);
+    }
+    setPitches(sortedPitches);
+    console.log(pitches);
+  }
   const getPitches = async () => {
     try {
       const response = await fetch(
@@ -33,7 +45,7 @@ export default function Main({navigation}) {
   };
 useEffect(() => {
   getPitches();
-}, );
+},[] );
 useEffect(() => {
   navigation.setOptions({
     drawerItemStyle: { display: 'auto' }
@@ -41,6 +53,20 @@ useEffect(() => {
 }, [navigation]);
   return (
     <SafeAreaView >
+      <RNPickerSelect
+          onValueChange={(value) => sortChange(value)}
+          title='Sort by'
+          items={[
+            { label: 'Price', value: 'price' },
+            { label: 'Rating', value: 'rating' },
+        ]}
+        style={pickerSelectStyles}
+        placeholder={{
+          label: 'Select a sorting option...',
+          value: null,
+          color: '#9EA0A4', // Placeholder text color
+        }}
+      />
       <FlatList data={pitches} renderItem={({ item }) => (
         <TouchableOpacity onPress={() =>{navigation.navigate("AddReservation",{item});}}>
           <PitchCard item={item} />
@@ -49,3 +75,26 @@ useEffect(() => {
     </SafeAreaView>
   );
 }
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30, 
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'purple',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30, 
+  },
+});
